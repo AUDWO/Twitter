facab94e8fd4978bf0defb0c050783e4d3784669
audwo <kmjstj3@naver.com>
2023-08-01 20:24:56 +0900

    Update AppLayout

---
+++ components/AppLayout.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import LoginForm from "./LoginForm";
+import styled from "styled-components";
+import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
+
+const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">노드버드</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">프로필</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <SearchInput enterButton />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">회원가입</Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </Col>
+        <Col xs={24} md={6}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.zerocho.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            made by zerocho
+          </a>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
